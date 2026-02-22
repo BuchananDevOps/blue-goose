a25fd13e073dad88ceb1ea3608ffcd97eda77d8f
BuchananDevOps <john@buchanandevops.com>
2026-02-22 14:31:16 -0500

    android and capacitor

---
+++ android/app/src/main/java/com/bluegoose/ecaller/MainActivity.java
@@ -0,0 +1,5 @@
+package com.bluegoose.ecaller;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bluegoose.ecaller',
+  appName: 'Blue Goose',
+  webDir: 'out',
+};
+
+export default config;
